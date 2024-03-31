02d5a201e3274ed458b20434397a42d9fac38719
Pranav Naik <nk.pranav99@gmail.com>
2024-03-31 15:31:49 +0000

    update

--- Tests/test_slider.py
+++ Tests/test_slider.py
@@ -4,14 +4,17 @@ from selenium.webdriver.common.by import By
 from Pages.BasePage import BasePage
 from Tests.common_tests import CommonTests
 from Utilities.test_data import TestData
+import time
 
 
 class TestScenario2(CommonTests):
-    def test_drag_and_drop_slider(self):
+    def test_slider(self):
         # We can use ActionChains to automate the drag and drop scenario
         base_page = BasePage(self.driver)
 
         base_page.click_on_element(TestData.drag_and_drop_slider_link)
+
+        time.sleep(5)
 
         # Locate the slider
         slider = base_page.driver.find_element(By.XPATH, TestData.default_value_15_slider)
